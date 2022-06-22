2e326cf0fda607623cb0a95397f1e1b9a784d0ac
Benedict Wetzel <benedict.wetzel@trumpf.com>
2022-06-22 12:22:24 +0200

    updates

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ function handle(event) {
 }
 
 if (typeof window.DeviceOrientationEvent.requestPermission === 'function') {
+    console.log("Device is iOS 13+")
     window.DeviceOrientationEvent.requestPermission()
         .then(response => {
+            console.log("Request Permission: ")
+            console.log(response)
             if (response == 'granted') {
                 window.addEventListener('deviceorientation', handle)
             }
         })
         .catch(console.error)
 } else {
+    console.log("Not iOS")
     window.addEventListener("deviceorientation", handle)
 }
