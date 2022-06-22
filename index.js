d933b1248b631d3fcf7939de883bc0a68dd26c83
Benedict Wetzel <benedict.wetzel@trumpf.com>
2022-06-22 11:22:25 +0200

    updates

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.addEventListener("deviceorientation", (event) => {
-    document.getElementById("show").innerText = event.toString()
+    document.getElementById("show").innerText = event.alpha
 })
