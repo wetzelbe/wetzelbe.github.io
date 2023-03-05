0561bfba562a06a3f011ff58df1653fde0a130c0
Benedict Wetzel <68902900+wetzelbe@users.noreply.github.com>
2023-03-05 19:32:08 +0100

    Update cat.js

--- games/cat.js
+++ games/cat.js
@@ -1,5 +1,46 @@
-function drawCat()
+// Global vars
+var catY = 0
+var catDirection = 0
+var obstacles = []
+var frame = 0
+var runnning = false
+var canvasId = ""
+
+function drawCat(canvas, height)
 {
+  var ctx = canvas.getContext("2d");
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+
+}
+
+
+function drawFrame()
+{
+  canvas = document.getElementbyId(canvasId)
+  drawCat(canvas, catY)
   
 
+  
+}
+
+
+const doPhysics = () => {
+}
+
+
+const tick = () => {
+  if (running)
+  {
+    doPhysics()
+    drawFrame()
+    setTimeout(tick, 50)
+  }
+}
+
+const start = () => {
+  running = true
+  tick()
+  
 }
