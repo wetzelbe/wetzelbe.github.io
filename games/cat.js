b03c341456976eed66024a451abf26cf64f67d14
Benedict Wetzel <68902900+wetzelbe@users.noreply.github.com>
2023-03-05 19:54:28 +0100

    Update cat.js

--- games/cat.js
+++ games/cat.js
@@ -6,13 +6,17 @@ var frame = 0
 var runnning = false
 var canvasId = "catgamecanvas"
 
-const drawCat = (canvas, height) => {
+const drawCat = (canvas, height, version) => {
   let ctx = canvas.getContext("2d");
   let startX = 30
   let startY = 300 - catY - 24
   
   ctx.fillStyle = "white"
-  ctx.fillRect(startX, startY, 2, 2);
+  ctx.fillRect(startX + 8, startY, 2, 2);
+  ctx.fillRect(startX + 18, startY, 2, 2);
+  ctx.fillRect(startX + 8, startY + 2, 4, 8);
+  ctx.fillRect(startX + 16, startY + 2, 4, 8);
+  ctx.fillRect(startX + 12, startY + 4, 4, 6);
   ctx.fillRect(startX + 2, startY + 12, 16, 8);
 
 }
